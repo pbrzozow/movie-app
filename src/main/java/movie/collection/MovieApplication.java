f1e95ff531752c2ca2c962c6bc4180a5b28a4e8b
PB <180855918+pbrzozow@users.noreply.github.com>
2025-02-20 15:22:12 +0100

    update MovieApplication

--- src/main/java/movie/collection/MovieApplication.java
+++ src/main/java/movie/collection/MovieApplication.java
@@ -1,9 +1,8 @@
 package movie.collection;
 
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 
-@ComponentScan
+@ComponentScan(basePackages = "movie.collection")
 public class MovieApplication {
     public static void main(String[] args) {
 
