cbe5c2c1113d327aa0b86a7d5d9c3607aec9d361
PB <180855918+pbrzozow@users.noreply.github.com>
2025-02-25 13:49:17 +0100

    add mail utils

---
+++ src/main/java/movie/collection/config/MailConfig.java
@@ -0,0 +1,34 @@
+package movie.collection.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("localhost");
+        mailSender.setPort(1025);
+
+        mailSender.setUsername("");
+        mailSender.setPassword("");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "false");
+        props.put("mail.smtp.starttls.enable", "false");
+        props.put("mail.smtp.ssl.trust", "*");
+        props.put("mail.smtp.connectiontimeout", "5000");
+        props.put("mail.smtp.timeout", "3000");
+        props.put("mail.smtp.writetimeout", "5000");
+
+        return mailSender;
+    }
+}
+
---
+++ src/main/java/movie/collection/service/EmailService.java
@@ -0,0 +1,37 @@
+package movie.collection.service;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendEmailWithLink(String to, String subject, String link) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+        helper.setFrom("test@example.com");
+        helper.setTo(to);
+        helper.setSubject(subject);
+
+        String htmlContent = "<p>Hello,</p>"
+                + "<p>Please click the link below to verify your account:</p>"
+                + "<p><a href=\"" + link + "\">Verify Account</a></p>"
+                + "<p>Thank you!</p>";
+
+        helper.setText(htmlContent, true);
+
+        mailSender.send(message);
+    }
+}
