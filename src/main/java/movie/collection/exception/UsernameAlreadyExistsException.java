d2232f44e0d54065f33f83df76400c7ac09699c6
PB <180855918+pbrzozow@users.noreply.github.com>
2025-02-25 09:37:16 +0100

    add custom exceptions

---
+++ src/main/java/movie/collection/exception/UsernameAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package movie.collection.exception;
+
+public class UsernameAlreadyExistsException extends RuntimeException {
+    public UsernameAlreadyExistsException(String message) {
+        super(message);
+    }
+}
