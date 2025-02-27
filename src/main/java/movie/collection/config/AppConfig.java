aec24cb46a0238edc9fef7860838cf057c071508
PB <180855918+pbrzozow@users.noreply.github.com>
2025-02-27 10:22:55 +0100

    first fix setup

--- src/main/java/movie/collection/config/AppConfig.java
+++ src/main/java/movie/collection/config/AppConfig.java
@@ -42,7 +42,7 @@ public class AppConfig {
         emf.setJpaVendorAdapter(vendorAdapter);
 
         Properties properties = new Properties();
-        properties.put("hibernate.hbm2ddl.auto", "update");
+        properties.put("hibernate.hbm2ddl.auto", "create-drop");
         properties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
         properties.put("hibernate.show_sql", "true");
         properties.put("hibernate.format_sql", "false");
--- src/main/java/movie/collection/service/MovieService.java
+++ src/main/java/movie/collection/service/MovieService.java
@@ -54,7 +54,69 @@ public class MovieService {
             movieRepository.save(movie);
         }
     }
+    public void initialize(){
+        Movie theGodfather = Movie.builder().icon("https://th.bing.com/th/id/OIP.3C9P6X2vrW-EGjNpsSMgyQHaK9?rs=1&pid=ImgDetMain")
+                .title("The godfather")
+                .rating(5.0)
+                .category("Drama")
+                .description("Gangster movie")
+                .duration(50)
+                .releaseYear(1972)
+                .watchedTimes(5)
+                .build();
+        Movie movie3= Movie.builder().icon("https://th.bing.com/th/id/OIP.3C9P6X2vrW-EGjNpsSMgyQHaK9?rs=1&pid=ImgDetMain")
+                .title("lama")
+                .rating(5.0)
+                .category("Drama")
+                .description("Gangster movie")
+                .duration(50)
+                .releaseYear(1972)
+                .watchedTimes(5)
+                .build();
+        Movie movie2 = Movie.builder().icon("https://th.bing.com/th/id/OIP.3C9P6X2vrW-EGjNpsSMgyQHaK9?rs=1&pid=ImgDetMain")
+                .title("gamma")
+                .rating(5.0)
+                .category("Drama")
+                .description("Gangster movie")
+                .duration(50)
+                .releaseYear(1972)
+                .watchedTimes(5)
+                .build();
+        Movie movie1 = Movie.builder().icon("https://th.bing.com/th/id/OIP.3C9P6X2vrW-EGjNpsSMgyQHaK9?rs=1&pid=ImgDetMain")
+                .title("The godfather")
+                .rating(5.0)
+                .category("Drama")
+                .description("Gangster movie")
+                .duration(50)
+                .releaseYear(1972)
+                .watchedTimes(5)
+                .build();
+        Movie movie4 = Movie.builder().icon("https://th.bing.com/th/id/OIP.3C9P6X2vrW-EGjNpsSMgyQHaK9?rs=1&pid=ImgDetMain")
+                .title("alfa")
+                .rating(5.0)
+                .category("Drama")
+                .description("Gangster movie")
+                .duration(50)
+                .releaseYear(1972)
+                .watchedTimes(5)
+                .build();
 
+        Movie movie5 = Movie.builder().icon("https://th.bing.com/th/id/OIP.3C9P6X2vrW-EGjNpsSMgyQHaK9?rs=1&pid=ImgDetMain")
+                .title("delta")
+                .rating(5.0)
+                .category("Drama")
+                .description("Gangster movie")
+                .duration(50)
+                .releaseYear(1972)
+                .watchedTimes(5)
+                .build();
+        movieRepository.save(theGodfather);
+        movieRepository.save(movie1);
+        movieRepository.save(movie2);
+        movieRepository.save(movie3);
+        movieRepository.save(movie4);
+        movieRepository.save(movie5);
+    }
 
 
 }
